7f6c61da2e814cfc10f55dfd29b608eec59628f2
Robert Baumeister <rpbaumeister@gmail.com>
2021-05-19 14:08:26 -0500

    Created all modules and most or all services
    Made all the modules that I anticipate needing, made all services.  If this is too many modules/services, we can purge a few later.  We may need/want a password-match-directive/validator.

---
+++ frontend/update24x7/src/app/components/adminregister/adminregister.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminregisterComponent } from './adminregister.component';
+
+describe('AdminregisterComponent', () => {
+  let component: AdminregisterComponent;
+  let fixture: ComponentFixture<AdminregisterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminregisterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminregisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
