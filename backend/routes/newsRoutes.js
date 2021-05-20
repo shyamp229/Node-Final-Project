86fc3ad1b723f53645315122838f3cb8d2ae3461
shyam <shyamp229@gmail.com>
2021-05-20 12:28:29 -0400

    added auth

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,85 +1,23 @@
 const router = require('express').Router();
-const News = require('../models/NewsSchema');
+const News = require('../controllers/NewsController')
+const { isAuthenticated } = require('../Auth/AuthController')
+// const News = require('../models/NewsSchema');
 
-
+// get
 router.get('/', (req, res) => {
     res.send("Router is running!!");
 });
+router.get('/allNews', News.allNews);
+router.get("/threeLatestNews", News.threeLatestNews);
+router.get("/getSingleNews/:id", News.getSingleNews);
 
-router.post('/addNews', (req, res) => {
+// post
+router.post('/addNews', isAuthenticated, News.addNews);
 
-    const addNews = new News();
+// update
+router.put('/editNews/:id', isAuthenticated, News.editNews);
 
-    addNews.title = req.body.title;
-    addNews.description = req.body.description;
-    addNews.url = req.body.url;
-    addNews.urlToImage = req.body.urlToImage;
-    addNews.publishedAt = req.body.publishedAt;
-
-    addNews.save()
-        .then(news => res.json(news))
-        .catch(err => res.status(500).json(err))
-
-})
-
-router.get('/allNews', (req, res) => {
-    News.find()
-        .then(news => res.json(news))
-        .catch(err => res.status(500).json(err))
-})
-
-router.get("/threeLatestNews", (req, res) => {
-    News.find()
-        .limit(3)
-        .sort({ publishedAt: -1 })
-        .then((news) => {
-            if (!news) {
-                return res.status(404).json({ error: "there is no data" });
-            }
-            console.log(JSON.stringify(news));
-            res.status(200).json(news);
-        })
-        .catch((e) => res.status(404).json(e));
-});
-
-router.get("/getSingleNews/:id", (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    News.findOne({ _id: id })
-        .then((news) => {
-            console.log(news);
-            if (!news) {
-                return res.status(404).json({ error: "there is no data" });
-            }
-            console.log(JSON.stringify(news));
-            res.status(200).json(news);
-        })
-        .catch((e) => res.status(404).json(e));
-});
-
-router.delete('/deleteNews/:id', (req, res) => {
-    // const { id } = req.params
-    const id = req.params.id;
-    News.findByIdAndDelete(id)
-        .then(data => res.json(data))
-        .catch(err => res.status(500).json(err))
-})
-
-router.put('/editNews/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    News.findOneAndUpdate({ id }, {
-        $set: {
-            title: req.body.title,
-            description: req.body.description,
-            url: req.body.url,
-            urlToImage: req.body.urlToImage,
-            publishedAt: req.body.publishedAt,
-        }
-    }, (err, result) => {
-        if (err) res.send('Error updating document!!');
-        else res.send({ message: 'Document updated successfully!!' });
-    })
-})
+// delete
+router.delete('/deleteNews/:id', isAuthenticated, News.deleteNews);
 
 module.exports = router;
