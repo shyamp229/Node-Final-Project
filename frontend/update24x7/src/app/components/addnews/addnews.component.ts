2a4219596b16f42cd618887b76fa217e64fa4f49
Robert Baumeister <rpbaumeister@gmail.com>
2021-05-20 12:29:48 -0500

    Incremental change
    Not really on right track.  Taking some items from previous assignments for scaffold

--- frontend/update24x7/src/app/components/addnews/addnews.component.ts
+++ frontend/update24x7/src/app/components/addnews/addnews.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-addnews',
-  templateUrl: './addnews.component.html',
-  styleUrls: ['./addnews.component.css']
-})
-export class AddnewsComponent implements OnInit {
 
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+
+  editIcon:any;
+  showEdit:boolean=false;
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.editIcon='assets/edit-512.png'
+
+    //this.employee = {
+      // employeeId:2,
+       // firstName:'John',
+       // LastName:'Smith',
+       // salary:10000,
+       // DOB:new Date('11/02/1983'),
+       //email:'john@abc.com'
+
+    //}
+
+
+
+    
+  }
+
+  EditEmp(){
+      this.showEdit = true;
+  }
+
+  UpdateEmp(){
+    this.showEdit=false;
+   
   }
 
 }
